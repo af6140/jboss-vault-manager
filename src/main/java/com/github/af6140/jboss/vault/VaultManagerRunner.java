d79b82c308b4cfb2662f865159cfaaa33ff88912
af6140 <daweiwang.gatekeeper@gmail.com>
2020-03-01 15:51:26 -0500

    readme and enable argument file

--- src/main/java/com/github/af6140/jboss/vault/VaultManagerRunner.java
+++ src/main/java/com/github/af6140/jboss/vault/VaultManagerRunner.java
@@ -1,8 +1,6 @@
 package com.github.af6140.jboss.vault;
 
-import com.github.af6140.jboss.vault.command.StoreSecret;
 import com.github.af6140.jboss.vault.command.Vault;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.stereotype.Component;
@@ -12,23 +10,23 @@ import picocli.CommandLine;
 @SuppressWarnings("unused")
 public class VaultManagerRunner implements CommandLineRunner, ExitCodeGenerator {
 
-    private final CommandLine.IFactory factory; // auto-configured to inject PicocliSpringFactory
-    private final Vault vault;
-    private int exitCode;
+  private final CommandLine.IFactory factory; // auto-configured to inject PicocliSpringFactory
+  private final Vault vault;
+  private int exitCode;
 
-    public VaultManagerRunner(Vault vault, CommandLine.IFactory factory) {
-        this.factory = factory;
-        this.vault= vault;
-    }
+  public VaultManagerRunner(Vault vault, CommandLine.IFactory factory) {
+    this.factory = factory;
+    this.vault = vault;
+  }
 
-    @Override
-    public void run(String... args) {
-        exitCode = new CommandLine(vault, factory)
-                .execute(args);
-    }
+  @Override
+  public void run(String... args) {
 
-    @Override
-    public int getExitCode() {
-        return exitCode;
-    }
+    exitCode = new CommandLine(vault, factory).setUseSimplifiedAtFiles(true).execute(args);
+  }
+
+  @Override
+  public int getExitCode() {
+    return exitCode;
+  }
 }
